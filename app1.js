4254b29d535a753e1d5003e858daaa4fd976e506
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-04-03 16:33:02 +0530

    keshav

--- app1.js
+++ app1.js
@@ -14,13 +14,23 @@ var storage = multer.diskStorage({
         cb(null, "Images");
     },
     filename: function (req, file, cb) {
-      //  let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+        console.log("req : ", req.KeshavSoft, file);
+        //  let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
         //cb(null, Date.now() + ext);
-        cb(null, Date.now() + path.extname(file.originalname));
+        //  cb(null, Date.now() + path.extname(file.originalname));
+        //cb(null, req.body.nspeakers + path.extname(file.originalname));
+
+        cb(null, req.KeshavSoft + path.extname(file.originalname));
+
     }
 })
 
 var upload = multer({ storage: storage });
+
+let kmiddle = (req, res, next) => {
+    req.KeshavSoft = "Keshav";
+    next();
+};
 
 app.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname + `/upload.html`));
@@ -31,10 +41,11 @@ app.get('/file', function (req, res, next) {
     res.sendFile(path.join(__dirname + `/Images/1680285347210.jpg`));
 });
 
-app.post('/stats', upload.single("uploaded_file"), function (req, res) {
+app.post('/stats', kmiddle, upload.single("uploaded_file"), function (req, res) {
     // req.file is the name of your file in the form above, here 'uploaded_file'
     // req.body will hold the text fields, if there were any 
-    console.log(req.file, req.body)
+    // console.log(req.file, req.body)
+    res.end("success");
 });
 
 function normalizePort(val) {
